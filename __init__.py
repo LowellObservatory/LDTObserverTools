3c1c1c646d40d6bfeb4a4d8564fcb51747876aa8
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2021-02-01 10:18:51 -0700

    Add package initialization file __init.py__
    This should make pydeveny importable as a package.
    
    	new file:   __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of PyDeVeny.
+#
+#   This Source Code Form is subject to the terms of the Mozilla Public
+#   License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  Created on 01-Feb-2021
+#
+#  @author: tbowers
+
+from .pydeveny import *
+
+# Boilerplate variables
+__author__ = 'Timothy P. Ellsworth Bowers'
+__copyright__ = 'Copyright 2021'
+__credits__ = ['Lowell Observatory']
+__license__ = 'MPL-2.0'
+__version__ = '0.1.0'
+__email__ = 'tbowers@lowell.edu'
+__status__ = 'Development Status :: 2 - Pre-Alpha'
