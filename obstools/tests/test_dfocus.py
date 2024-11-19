985c30d879803726db9653026fced4d9ecbbd417
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2024-11-18 21:43:27 -0700

    Add dfocus test file
    	new file:   obstools/tests/test_dfocus.py

---
+++ obstools/tests/test_dfocus.py
@@ -0,0 +1,67 @@
+# pylint: disable=missing-function-docstring
+# -*- coding: utf-8 -*-
+#
+#  This file is part of LDTObserverTools.
+#
+#   This Source Code Form is subject to the terms of the Mozilla Public
+#   License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  Created on 18-Nov-2024
+#
+#  @author: tbowers
+
+"""DeVeny Collimator Focus Calculator TEST Module
+
+NOTE: Use unit testing as a guide for rewriting the dfocus module in a more
+      pythonic, less IDL style.  In particular, think about breaking down the
+      functions into functionality rather than narrative steps.
+"""
+
+import numpy as np
+
+from obstools import dfocus
+
+
+def test_dfocus():
+    pass
+
+
+def test_initialize_focus_values():
+    pass
+
+
+def test_parse_focus_log():
+    pass
+
+
+def test_process_middle_image():
+    pass
+
+
+def test_trim_deveny_image():
+    pass
+
+
+def test_extract_spectrum():
+    pass
+
+
+def test_find_lines():
+    pass
+
+
+def test_fit_focus_curves():
+    pass
+
+
+def test_plot_optimal_focus():
+    pass
+
+
+def test_plot_focus_curves():
+    pass
+
+
+def test_find_lines_in_spectrum():
+    pass
